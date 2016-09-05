fdc4effef4f1b2423834d0619e883e9260ef382b
Marten Veldthuis <marten@veldthuis.com>
2016-09-05 14:10:58 +0100

    temporary enable debug info in prod mode

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :cellect, Cellect.Endpoint,
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 config :cellect, Cellect.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
