5c27193e9110bfe743386813f4ac9651b1850568
Marten Veldthuis <marten@veldthuis.com>
2016-09-06 12:04:54 +0100

    Fix for anonymous users

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,6 +1,7 @@
 defmodule Cellect.User do
   import Ecto.Query, only: [from: 2]
 
+  def seen_subject_ids(workflow_id, nil), do: []
   def seen_subject_ids(workflow_id, user_id) do
     query = from uss in "user_seen_subjects",
       where: uss.workflow_id == ^workflow_id and uss.user_id == ^user_id,
