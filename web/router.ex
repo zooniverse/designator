61cd635d4fb801a0125dcf9889acdc69630a10c4
Marten Veldthuis <marten@veldthuis.com>
2017-02-13 16:11:35 +0000

    Add plug to catch any web-request errors

--- web/router.ex
+++ web/router.ex
@@ -1,5 +1,6 @@
 defmodule Cellect.Router do
   use Cellect.Web, :router
+  use Plug.ErrorHandler
 
   pipeline :api do
     plug :accepts, ["json"]
@@ -12,4 +13,9 @@ defmodule Cellect.Router do
     post "/workflows/:workflow_id/reload", WorkflowsController, :reload
     put  "/workflows/:workflow_id/remove", WorkflowsController, :retire
   end
+
+  defp handle_errors(conn, %{kind: kind, reason: reason, stack: stacktrace}) do
+    Rollbax.report(kind, reason, stacktrace)
+  end
 end
+
