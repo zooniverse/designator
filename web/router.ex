7787cf0d0e623ef5f1c0d93cb0d4b827cd18f64b
Marten Veldthuis <marten@veldthuis.com>
2017-02-20 13:10:40 +0000

    Just route into the reload action

--- web/router.ex
+++ web/router.ex
@@ -11,7 +11,7 @@ defmodule Cellect.Router do
 
     get  "/workflows/:workflow_id", WorkflowsController, :index
     post "/workflows/:workflow_id/reload", WorkflowsController, :reload
-    post "/workflows/:workflow_id/remove", WorkflowsController, :retire
+    post "/workflows/:workflow_id/remove", WorkflowsController, :reload
   end
 
   defp handle_errors(conn, %{kind: kind, reason: reason, stack: stacktrace}) do
