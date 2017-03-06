393698b94241274ba69d0e0d33da184109c87f21
Marten Veldthuis <marten@veldthuis.com>
2017-03-06 11:35:40 +0000

    Fix tests

--- test/controllers/workflow_controller_test.exs
+++ test/controllers/workflow_controller_test.exs
@@ -4,30 +4,39 @@ defmodule Designator.WorkflowControllerTest do
   alias Designator.Workflow
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    workflow = Repo.insert! %Workflow{}
+    {:ok, [workflow: workflow, conn: put_req_header(conn, "accept", "application/json")]}
   end
 
-  test "shows chosen resource", %{conn: conn} do
-    workflow = Repo.insert! %Workflow{}
-    conn = get conn, workflow_path(conn, :show, workflow)
+  test "shows chosen resource", %{workflow: workflow, conn: conn} do
+    conn = conn
+    |> get(workflow_path(conn, :show, workflow))
     assert json_response(conn, 200) == []
   end
 
-  test "reloads a workflow", %{conn: conn} do
-    workflow = Repo.insert! %Workflow{}
-    conn = post conn, workflow_path(conn, :reload, workflow)
+  test "reloads a workflow", %{workflow: workflow, conn: conn} do
+    conn = conn
+    |> http_basic_authenticate("username", "password")
+    |> post(workflow_path(conn, :reload, workflow))
     assert response(conn, 204) == ""
   end
 
-  test "unlocks a workflow reload", %{conn: conn} do
-    workflow = Repo.insert! %Workflow{}
-    conn = post conn, workflow_path(conn, :unlock, workflow)
+  test "unlocks a workflow reload", %{workflow: workflow, conn: conn} do
+    conn = conn
+    |> http_basic_authenticate("username", "password")
+    |> post(workflow_path(conn, :unlock, workflow))
     assert response(conn, 204) == ""
   end
 
-  test "marks a subject as retired", %{conn: conn} do
-    workflow = Repo.insert! %Workflow{}
-    conn = post conn, workflow_path(conn, :remove, workflow, subject_id: 123)
+  test "marks a subject as retired", %{workflow: workflow, conn: conn} do
+    conn = conn
+    |> http_basic_authenticate("username", "password")
+    |> post(workflow_path(conn, :remove, workflow, subject_id: 123))
     assert response(conn, 204) == ""
   end
+
+  def http_basic_authenticate(conn, username, password) do
+    put_req_header(conn, "authorization", "Basic " <> Base.encode64(username <> ":" <> password))
+  end
+
 end
