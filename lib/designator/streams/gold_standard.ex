66a8c54ca7a98805b943fbf358bd1d5fa8149f02
Marten Veldthuis <marten@veldthuis.com>
2017-03-09 16:33:43 +0000

    Set stream amounts on gold streams

--- lib/designator/streams/gold_standard.ex
+++ lib/designator/streams/gold_standard.ex
@@ -7,11 +7,15 @@ defmodule Designator.Streams.GoldStandard do
     gold_standard_set_ids = workflow.configuration["gold_standard_sets"] || []
 
     if Enum.count(gold_standard_set_ids) > 0 do
-      gold_stream = streams |> Enum.filter(&Enum.member?(gold_standard_set_ids, &1.subject_set_id)) |> StreamTools.interleave
-      test_stream = streams |> Enum.reject(&Enum.member?(gold_standard_set_ids, &1.subject_set_id)) |> StreamTools.interleave
+      gold_streams = streams |> Enum.filter(&Enum.member?(gold_standard_set_ids, &1.subject_set_id))
+      test_streams = streams |> Enum.reject(&Enum.member?(gold_standard_set_ids, &1.subject_set_id))
+      gold_stream_amount = Enum.sum(Enum.map(gold_streams, &(&1.amount)))
+      test_stream_amount = Enum.sum(Enum.map(test_streams, &(&1.amount)))
+      gold_stream = gold_streams |> StreamTools.interleave
+      test_stream = test_streams |> StreamTools.interleave
 
-      gold = %SubjectStream{stream: gold_stream, chance: gold_chance(Enum.count(seen_subject_ids))}
-      test = %SubjectStream{stream: test_stream, chance: 1 - gold_chance(Enum.count(seen_subject_ids))}
+      gold = %SubjectStream{stream: gold_stream, amount: gold_stream_amount, chance: gold_chance(Enum.count(seen_subject_ids))}
+      test = %SubjectStream{stream: test_stream, amount: test_stream_amount, chance: 1 - gold_chance(Enum.count(seen_subject_ids))}
       [gold, test]
     else
       streams
