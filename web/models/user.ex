5ef73f059f62bed246ac00535d430fe92aba875c
Marten Veldthuis <marten@veldthuis.com>
2017-10-23 14:54:25 +0100

    Ignore connection errors when loading user seens (#41)

--- web/models/user.ex
+++ web/models/user.ex
@@ -17,11 +17,14 @@ defmodule Designator.User do
 
   def seen_subject_ids(_workflow_id, nil), do: []
   def seen_subject_ids(workflow_id, user_id) do
-    query = from uss in "user_seen_subjects",
-      where: uss.workflow_id == ^workflow_id and uss.user_id == ^user_id,
-      select: uss.subject_ids
+    try do
+      query = from uss in "user_seen_subjects",
+        where: uss.workflow_id == ^workflow_id and uss.user_id == ^user_id,
+        select: uss.subject_ids
 
-    Designator.Repo.one(query) || []
+      Designator.Repo.one(query) || []
+    rescue
+      DBConnection.ConnectionError -> []
+    end
   end
-
 end
