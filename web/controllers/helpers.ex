d1c196c26c0c23536c15cca4176938485a65b0fc
Campbell Allen <campbell.allen@gmail.com>
2019-07-04 14:53:14 +0100

    add get int param functions

--- web/controllers/helpers.ex
+++ web/controllers/helpers.ex
@@ -18,12 +18,24 @@ defmodule Designator.Controllers.Helpers do
     end
   end
 
-  defp get_integer_param(params, key, default) do
+  def get_integer_param(params, key, default) do
     case Map.get(params, key) do
       nil -> default
-      value ->
-        {int, _} = Integer.parse(value)
-        int
+      value -> convert_to_int(value)
     end
+  end
+
+  def convert_to_int(value) when is_integer(value) do
+    value
+  end
+
+  def convert_to_int(value) when is_binary(value) do
+    {int_value, _} = Integer.parse(value)
+    int_value
+  end
+
+  def convert_to_int(value) when is_list(value) do
+    {int_value, _} = Integer.parse(to_string(value))
+    int_value
   end
 end
