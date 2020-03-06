4442ae1141e4a614cebd59de56dbbb70be24131e
Campbell Allen <campbell.allen@gmail.com>
2020-03-06 14:35:34 +0000

    switch to http server for deployment (#97)
    * switch to http server for deployment
    
    allow port to be configurable for dev & prod
    
    run server on http
    
    use docker CMD start script with ENV var configuration opts for erlang and elixir
    
    avoid ssl setup on boot
    
    allow docker compose to run the tests via the built image
    
    * fix travis runs
    
    * only run on master or prs
    
    * use shell exec vs forking
    
    Co-Authored-By: Adam McMaster <adam@zooniverse.org>
    
    * add an overridable default port for app
    
    Co-authored-by: Adam McMaster <adam@zooniverse.org>

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM elixir:1.8
 
-# ENV LANG en_US.UTF-8
-# ENV LANGUAGE en_US:en
-# ENV LC_ALL en_US.UTF-8
 ENV MIX_ENV prod
 
 ADD . /app
@@ -11,4 +8,4 @@ RUN mix local.hex --force
 RUN mix local.rebar --force
 RUN mix deps.get
 RUN mix compile
-CMD ["bash", "entrypoint.sh"]
+CMD ["/app/start.sh"]
---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ensure we stop on error (-e) and log cmds (-x)
+set -ex
+ERLANG_NODE_NAME=${ERLANG_NODE_NAME:-erlang@designator.zooniverse.org}
+ERLANG_DISTRIBUTED_PORT=${ERLANG_DISTRIBUTED_PORT:-9001}
+# enable the SMP (multiprocessing) for erlang VM, https://docs.riak.com/riak/kv/latest/using/performance/erlang/index.html#smp
+ERLANG_SMP=${ERLANG_SMP:-auto}
+ERLANG_KERNEL_OPTS=${ERLANG_KERNEL_OPTS:-"inet_dist_listen_min ${ERLANG_DISTRIBUTED_PORT} inet_dist_listen_max ${ERLANG_DISTRIBUTED_PORT}"}
+ERLANG_OPTS="-smp ${ERLANG_SMP} -kernel ${ERLANG_KERNEL_OPTS}"
+PORT=${PORT:-80}
+# run the elixir app via Erlang VM
+exec env PORT=$PORT elixir --name $ERLANG_NODE_NAME --cookie $SECRET_KEY_BASE --erl "${ERLANG_OPTS}" -S mix phoenix.server
