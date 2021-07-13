16101cac0bd09343f5af7cf235729d8d81305cee
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2021-07-13 15:10:37 -0500

    adding random_stream replacement tests

--- lib/designator/subject_set_iterators/randomly.ex
+++ lib/designator/subject_set_iterators/randomly.ex
@@ -1,4 +1,24 @@
 defmodule Designator.SubjectSetIterators.Randomly do
   alias Designator.Random
-  
+
+  @spec apply_to(Enumerable.t) :: Enumerable.t
+  def apply_to(enum) do
+    Stream.unfold({enum, MapSet.new}, fn {enum, drawn} ->
+      if size(enum) <= MapSet.size(drawn) do
+        nil
+      else
+        {index, element} = Random.unique_element(enum, drawn)
+
+        {{index, element}, {enum, MapSet.put(drawn, index)}}
+      end
+    end)
+  end
+
+  defp size(enum = %Array{}) do
+    Array.size(enum)
+  end
+
+  defp size(enum) do
+    Enum.count(enum)
+  end
 end
---
+++ test/designator/subject_set_iterators/randomly_test.exs
@@ -0,0 +1,13 @@
+defmodule Designator.SubjectSetIterators.RandomlyTest do
+  use ExUnit.Case
+
+  import Designator.SubjectSetIterators.Randomly
+
+  test "empty enum returns nothing" do
+    assert ([] |> apply_to |> Stream.take(5) |> Enum.sort) == []
+  end
+
+  test "returns data" do
+    assert (1..5 |> apply_to |> Stream.take(5) |> Enum.sort) == [{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}]
+  end
+end
